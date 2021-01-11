86ca6f39ffc5347743042eceb5dcee29a7c4aa1f
Albert Romera Mulero <albert.romera.arm@gmail.com>
2021-01-11 20:29:00 +0100

    Add gitignore

---
+++ controllers/status_test.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHealthCheckController_HealthCheck(t *testing.T) {
+	// Setup
+	req := httptest.NewRequest(http.MethodGet, "/", strings.NewReader(""))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	rec := httptest.NewRecorder()
+	c := echo.New().NewContext(req, rec)
+
+	expected := `{"data":"¡Alive!"}`
+
+	sut := NewHealthCheckController()
+
+	// Assertions
+	if assert.NoError(t, sut.HealthCheck(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, expected, rec.Body.String())
+	}
+}
